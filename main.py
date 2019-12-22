868709a3d4f4ba765e1053595bf7e90f9df03541
Antonius Anggito <38179060+Gytzero@users.noreply.github.com>
2019-12-23 00:22:45 +0700

    Fixed HP display after battle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import math
+import weakref
 
 class Moves():
     def __init__(self, movename, atk):
@@ -17,11 +18,18 @@ class Moves():
 
 
 class UniquePoke():
+
+    pokemonlist = []
+
     def __init__(self, name, poketype, **move):
+        self.__class__.pokemonlist.append(weakref.proxy(self))
         self.name = name
-        self.type = poketype
+        self.poketype = poketype
         self.xp = 0
         self.movedict = move
+
+    def __str__(self):
+        return self.name
 
 
 class MyPokemon(UniquePoke):
@@ -32,19 +40,17 @@ class MyPokemon(UniquePoke):
         self.hp = self.lvl*2 + 2
         self.xp = xp
         self.xplimit = self.lvl * 5
-
-    def assignMoves(self):
-        self.newmovedict = dict((int(key[3:]), value) for (key, value) in self.pokeobj.movedict.items())
-        self.assmove = []
-        for k,v in self.newmovedict.items():
-            if k <= self.lvl and len(self.assmove) <= 4:
-                self.assmove.append(v)
-        while len(self.assmove) < 4:
-            self.assmove.append("")
-        self.move1 = self.assmove[0]
-        self.move2 = self.assmove[1]
-        self.move3 = self.assmove[2]
-        self.move4 = self.assmove[3]
+        newmovedict = dict((int(key[3:]), value) for (key, value) in self.pokeobj.movedict.items())
+        movelist = []
+        for k,v in newmovedict.items():
+            if k <= self.lvl and len(movelist) <= 4:
+                movelist.append(v)
+        while len(movelist) < 4:
+            movelist.append("")
+        self.move1 = movelist[0]
+        self.move2 = movelist[1]
+        self.move3 = movelist[2]
+        self.move4 = movelist[3]
 
     def getMoves(self):
         return f"Moves available:\n 1.{self.move1}\n 2.{self.move2}\n 3.{self.move3}\n 4.{self.move4}"
@@ -52,16 +58,16 @@ class MyPokemon(UniquePoke):
     def doMove(self, movenum):
         self.movenum = movenum
         if self.movenum == '1':
-            print(f"{self.pokeobj.name} uses {self.move1} on foe's pokemon!")
+            print(f"Your {self.pokeobj.name} used {self.move1} on the foe's pokemon!")
             return self.move1.atk
         elif self.movenum == '2':
-            print(f"{self.pokeobj.name} uses {self.move2} on foe's pokemon!")
+            print(f"Your {self.pokeobj.name} used {self.move2} on the foe's pokemon!")
             return self.move2.atk
         elif self.movenum == '3':
-            print(f"{self.pokeobj.name} uses {self.move3} on foe's pokemon!")
+            print(f"Your {self.pokeobj.name} used {self.move3} on the foe's pokemon!")
             return self.move3.atk
         elif self.movenum == '4':
-            print(f"{self.pokeobj.name} uses {self.move4} on foe's pokemon!")
+            print(f"Your {self.pokeobj.name} used {self.move4} on the foe's pokemon!")
             return self.move4.atk
 
     def hpLoss(self, hitdmg):
@@ -71,7 +77,7 @@ class MyPokemon(UniquePoke):
         return self.hp
 
     def showHp(self):
-        print(f"{self.pokeobj.name}'s HP is now {self.hp}.")
+        print(f"*Your {self.pokeobj.name} HP is now {self.hp}.")
 
     def gainXp(self, xpgain):
         self.xp += xpgain
@@ -103,33 +109,32 @@ class WildPokemon(UniquePoke):
         self.lvl = lvl
         self.yieldXp = self.lvl * 2
         self.hp = self.lvl*2 + 2
-
-    def assignMoves(self):
-        self.newmovedict = dict((int(key[3:]), value) for (key, value) in self.pokeobj.movedict.items())
-        self.assmove = []
-        for k,v in self.newmovedict.items():
-            if k <= self.lvl and len(self.assmove) <= 4:
-                self.assmove.append(v)
-        while len(self.assmove) < 4:
-            self.assmove.append("")
-        self.move1 = self.assmove[0]
-        self.move2 = self.assmove[1]
-        self.move3 = self.assmove[2]
-        self.move4 = self.assmove[3]
+        # Assigning moveset
+        newmovedict = dict((int(key[3:]), value) for (key, value) in self.pokeobj.movedict.items())
+        movelist = []
+        for k,v in newmovedict.items():
+            if k <= self.lvl and len(movelist) <= 4:
+                movelist.append(v)
+        while len(movelist) < 4:
+            movelist.append("")
+        self.move1 = movelist[0]
+        self.move2 = movelist[1]
+        self.move3 = movelist[2]
+        self.move4 = movelist[3]
 
     def doMove(self):
         self.movenum = str(random.randint(1, 2))
         if self.movenum == '1':
-            print(f"{self.pokeobj.name} uses {self.move1} on your pokemon!")
+            print(f"Foe's {self.pokeobj.name} used {self.move1} on your pokemon!")
             return self.move1.atk
         elif self.movenum == '2':
-            print(f"{self.pokeobj.name} uses {self.move2} on your pokemon!")
+            print(f"Foe's {self.pokeobj.name} used {self.move2} on your pokemon!")
             return self.move2.atk
         elif self.movenum == '3':
-            print(f"{self.pokeobj.name} uses {self.move3} on your pokemon!")
+            print(f"Foe's {self.pokeobj.name} used {self.move3} on your pokemon!")
             return self.move3.atk
         elif self.movenum == '4':
-            print(f"{self.pokeobj.name} uses {self.move4} on your pokemon!")
+            print(f"Foe's {self.pokeobj.name} used {self.move4} on your pokemon!")
             return self.move4.atk
 
     def hpLoss(self, hitdmg):
@@ -139,7 +144,7 @@ class WildPokemon(UniquePoke):
         return self.hp
 
     def showHp(self):
-        print(f"{self.pokeobj.name}'s HP is now {self.hp}.")
+        print(f"*Foe's {self.pokeobj.name} HP is now {self.hp}.")
         
     def __str__(self):
         return f"A wild lvl {self.lvl} {self.pokeobj.name} appeared!"
@@ -159,18 +164,17 @@ Charmander = UniquePoke("Charmander", "Fire", lvl2=Scratch, lvl5=Metal_Claw, lvl
 
 def main():
 
+    # Greetings
+    print("-Welcome to The Pokemon World!-")
+    print("-------------------------------")
+
     # Pokemon initiation
     my_pokemon = MyPokemon(Bulbasaur, lvl=5, xp=0)
-    wild_pokemon = WildPokemon(Charmander, lvl=6)
-    my_pokemon.assignMoves()
-    wild_pokemon.assignMoves()
+    wild_pokemon = WildPokemon(random.choice(UniquePoke.pokemonlist), lvl=6)
 
     # Battle scene
     exit = False
     while not exit:
-        print("-Welcome to The Pokemon World!-")
-        print("-------------------------------")
-
         # Print current pokemon state
         print(my_pokemon)
         print(wild_pokemon)
@@ -188,13 +192,21 @@ def main():
             # Damage calculations
             mydmg = my_pokemon.doMove(choose_move)
             wild_pokemon.hpLoss(mydmg)
-            foedmg = wild_pokemon.doMove()
-            my_pokemon.hpLoss(foedmg)
-            # Print current state
-            my_pokemon.showHp()
-            wild_pokemon.showHp()
-            print("------------------------------")
+            # When the foe survives
+            if wild_pokemon.getHp() > 0:
+                wild_pokemon.showHp()
+                foedmg = wild_pokemon.doMove()
+                my_pokemon.hpLoss(foedmg)
+                if my_pokemon.getHp() <= 0:
+                    # Exit the battle
+                    print("Your pokemon has fainted.")
+                    print("You have been thrown out of the game")
+                    exit = True
+                elif my_pokemon.getHp() > 0:
+                    my_pokemon.showHp()
+                    print("------------------------------")
 
+        # When the foe faints
         if wild_pokemon.getHp() <= 0:
             # Calculate xp and level gains
             print("The foe's pokemon has fainted!")
@@ -203,28 +215,24 @@ def main():
             valid = False
             while not valid:
                 # Asking for user's input to continue
-                quit = input("Keep going? [Y/n] ").lower()
-                if quit == 'n':
+                stay = input("Keep going? [Y/n] ").lower()
+                if stay == 'n':
                     # Exit the game
                     print("\n~Have a nice day!~")
                     exit = True
                     valid = True
-                elif quit == 'y':
+                elif stay == 'y':
                     # Initiate current state pokemon and moves
-                    my_pokemon = MyPokemon(Bulbasaur, lvl_now, xp_now)
-                    wild_pokemon= WildPokemon(Charmander, 6)
-                    my_pokemon.assignMoves()
-                    wild_pokemon.assignMoves()
+                    my_pokemon = MyPokemon(Bulbasaur, lvl=lvl_now, xp=xp_now)
+                    wild_pokemon= WildPokemon(Charmander, lvl=6)
                     valid = True
+                    print()
+                    print("------------------------------")
+                    print("------------------------------")
+                    print()
                 else:
                     pass
 
-        elif my_pokemon.getHp() <= 0:
-            # Exit the battle
-            print("Your pokemon has fainted.")
-            print("You have been thrown out of the game")
-            exit = True
-        
 
 if __name__ == '__main__':
     main()
