a8b809fc467fe7d5d76edb6d3bdc1f76f15630f4
Antonius Anggito <38179060+Gytzero@users.noreply.github.com>
2019-12-24 18:32:34 +0700

    Added area feature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 import weakref
 import time
 
+
 class Moves():
     def __init__(self, movename, pwr=0):
         self.movename = movename
@@ -22,7 +23,7 @@ class UniquePoke():
 
     pokemonlist = []
 
-    def __init__(self, name, poketype, bhp, batk, bdef, **move):
+    def __init__(self, name, poketype, bhp, batk, bdef, movedict):
         self.__class__.pokemonlist.append(weakref.proxy(self))
         self.name = name
         self.poketype = poketype
@@ -30,7 +31,7 @@ class UniquePoke():
         self.bhp = bhp
         self.batk = batk
         self.bdef = bdef
-        self.movedict = move
+        self.movedict = movedict
 
     def __str__(self):
         return self.name
@@ -47,23 +48,21 @@ class MyPokemon(UniquePoke):
         self.sdef = math.floor((((self.pokeobj.bdef*2) * self.lvl) / 100) + 5)
         self.xp = xp
         self.xplimit = self.lvl * 5
-        newmovedict = dict((int(key[3:]), value) for (key, value) in self.pokeobj.movedict.items())
-        movelist = []
-        for k,v in newmovedict.items():
-            if k <= self.lvl and len(movelist) <= 4:
-                movelist.append(v)
-        while len(movelist) < 4:
-            movelist.append("")
-        self.move1 = movelist[0]
-        self.move2 = movelist[1]
-        self.move3 = movelist[2]
-        self.move4 = movelist[3]
+        self.movelist = []
+        for k,v in self.pokeobj.movedict.items():
+            if k <= self.lvl and len(self.movelist) <= 4:
+                self.movelist.append(v)
+        while len(self.movelist) < 4:
+            self.movelist.append("")
+        self.move1 = self.movelist[0]
+        self.move2 = self.movelist[1]
+        self.move3 = self.movelist[2]
+        self.move4 = self.movelist[3]
 
-    def getMoves(self):
-        return f"Moves available:\n 1.{self.move1}\n 2.{self.move2}\n 3.{self.move3}\n 4.{self.move4}"
+    def showMoves(self):
+        print("Moves available:\n 1.{:20} 3.{}\n 2.{:20} 4.{}".format(str(self.move1), str(self.move3), str(self.move2), str(self.move4)))
 
     def doMove(self, foeobj, movenum):
-        movenum
         if movenum == '1':
             print(f"Your {self.pokeobj.name} used {self.move1} on the foe's pokemon!")
             mydmg = math.floor(((((2*self.lvl)/5 + 2) * self.move1.pwr * self.satk/foeobj.sdef) / 50) + 2)
@@ -99,11 +98,10 @@ class MyPokemon(UniquePoke):
     #     return self.xp
     
     def evalStat(self):
-        print(self.xp, self.xplimit)
+        print("Current exp:", self.xp, "from", self.xplimit)
         if self.xp >= self.xplimit:
             self.lvl += 1
-            self.xp = self.xp % self.xplimit
-            print(self.xp)
+            self.xp %= self.xplimit
             print(f"Your pokemon grew to lvl {self.lvl}!")
             return self.lvl, self.xp
         else:
@@ -111,6 +109,9 @@ class MyPokemon(UniquePoke):
 
     def __str__(self):
         return f"You have a lvl {self.lvl} {self.pokeobj.name}."
+
+    def __repr__(self):
+        return self.name
 
 
 class WildPokemon(UniquePoke):
@@ -120,27 +121,30 @@ class WildPokemon(UniquePoke):
         self.lvl = lvl
         self.yieldXp = (self.lvl * 2) + 2
         # Calculate its stat
-        self.shp = self.hp = math.floor((((self.pokeobj.bhp*2) * self.lvl) / 100) + self.lvl + 10)
+        self.shp = math.floor((((self.pokeobj.bhp*2) * self.lvl) / 100) + self.lvl + 10)
         self.satk = math.floor((((self.pokeobj.batk*2) * self.lvl) / 100) + 5)
         self.sdef = math.floor((((self.pokeobj.bdef*2) * self.lvl) / 100) + 5)
         # Assigning moveset
-        newmovedict = dict((int(key[3:]), value) for (key, value) in self.pokeobj.movedict.items())
-        movelist = []
-        for k,v in newmovedict.items():
-            if k <= self.lvl and len(movelist) <= 4:
-                movelist.append(v)
-        while len(movelist) < 4:
-            movelist.append("")
-        self.move1 = movelist[0]
-        self.move2 = movelist[1]
-        self.move3 = movelist[2]
-        self.move4 = movelist[3]
+        self.movelist = []
+        for k,v in self.pokeobj.movedict.items():
+            if k <= self.lvl and len(self.movelist) <= 4:
+                self.movelist.append(v)
+        while len(self.movelist) < 4:
+            self.movelist.append("")
+        self.move1 = self.movelist[0]
+        self.move2 = self.movelist[1]
+        self.move3 = self.movelist[2]
+        self.move4 = self.movelist[3]
 
-    def getMoves(self):
-        return f"Foe's moves:\n 1.{self.move1}\n 2.{self.move2}\n 3.{self.move3}\n 4.{self.move4}"
+    # def showMoves(self):
+    #     return f"Foe's moves:\n 1.{self.move1}\n 2.{self.move2}\n 3.{self.move3}\n 4.{self.move4}"
 
     def doMove(self, myobj):
-        movenum = str(random.randint(1, 2))
+        count = 0
+        for move in self.movelist:
+            if move != "":
+                count += 1
+        movenum = str(random.randint(1, count))
         if movenum == '1':
             print(f"Foe's {self.pokeobj.name} used {self.move1} on your pokemon!")
             foedmg = math.floor(((((2*self.lvl)/5 + 2) * self.move1.pwr * self.satk/myobj.sdef) / 50) + 2)
@@ -172,6 +176,18 @@ class WildPokemon(UniquePoke):
     def __str__(self):
         return f"A wild lvl {self.lvl} {self.pokeobj.name} appeared!"
 
+    def __repr__(self):
+        return self.name
+
+class Area():
+    def __init__(self, areaname, wildict):
+        self.areaname = areaname
+        self.wildict = wildict
+    
+    def __str__(self):
+        return self.areaname
+
+
 # Moves initiation
 Tackle = Moves("Tackle", 40)
 Scratch = Moves("Scratch", 40)
@@ -186,11 +202,15 @@ Thunder_Shock = Moves("Thunder Shock", 45)
 Slam = Moves("Slam", 50)
 
 # Pokemon available initiation
-Pikachu = UniquePoke("Pikachu", "Electric", bhp=35, batk=55, bdef=40, lvl2=Scratch, lvl4=Thunder_Shock, lvl7=Slam)
-Treecko = UniquePoke("Treecko", "Grass", bhp=40, batk=45, bdef=35, lvl2=Tackle, lvl4=Peck, lvl7=Vine_Whip)
-Torchic = UniquePoke("Torchic", "Fire", bhp=45, batk=60, bdef=40, lvl2=Scratch, lvl4=Metal_Claw, lvl7=Ember)
-Mudkip = UniquePoke("Mudkip", "Water", bhp=50, batk=70, bdef=50, lvl2=Pound, lvl4=Mud_Shot, lvl7=Bubble)
+Pikachu = UniquePoke("Pikachu", "Electric", bhp=35, batk=55, bdef=40, movedict={2:Scratch, 4:Thunder_Shock, 7:Slam})
+Treecko = UniquePoke("Treecko", "Grass", bhp=40, batk=45, bdef=35, movedict={2:Tackle, 4:Peck, 7:Vine_Whip})
+Torchic = UniquePoke("Torchic", "Fire", bhp=45, batk=60, bdef=40, movedict={2:Scratch, 4:Metal_Claw, 7:Ember})
+Mudkip = UniquePoke("Mudkip", "Water", bhp=50, batk=70, bdef=50, movedict={2:Pound, 4:Mud_Shot, 7:Bubble})
+Rattata = UniquePoke("Rattata", "Normal", bhp=30, batk=30, bdef=30, movedict={2:Tackle, 2:Scratch, 4:Pound})
+Pidgey = UniquePoke("Pidgey", "Flying", bhp=40, batk=40, bdef=40, movedict={2:Scratch, 2:Peck, 4:Slam})
 
+# Area initiation
+Route_1 = Area("Route 1", {Rattata:[2,4], Pidgey:[2,4]})
 
 def main():
 
@@ -199,9 +219,16 @@ def main():
     print("-----------------------------")
     time.sleep(0.5)
 
+    # Ask for area
+    area = Route_1
+    print(f"You are in {area}\n")
+    print(area.wildict)
+
     # Pokemon initiation
     my_pokemon = MyPokemon(Pikachu, lvl=5, xp=0)
-    wild_pokemon = WildPokemon(random.choice(UniquePoke.pokemonlist), lvl=4)
+    random_poke = random.choice(list(area.wildict))
+    lvl_min, lvl_max = area.wildict[random_poke]
+    wild_pokemon = WildPokemon(random_poke, lvl=random.randint(lvl_min, lvl_max))
 
     # Battle scene
     exit = False
@@ -216,10 +243,15 @@ def main():
         # First alternate battle
         while my_pokemon.getHp() > 0 and wild_pokemon.getHp() > 0:
             print()
-            print(my_pokemon.getMoves())
+            my_pokemon.showMoves()
             # Asking for user's intended move
             choose_move = ''
-            while choose_move not in ['1', '2', '3', '4']:
+            count = 0
+            for move in my_pokemon.movelist:
+                if move != "":
+                    count += 1
+            # Limitting undesired input
+            while choose_move not in [str(num+1) for num in range(count)]:
                 print("Which move would you do?")
                 choose_move = input("> ")
             time.sleep(0.5)
@@ -248,6 +280,7 @@ def main():
         if wild_pokemon.getHp() <= 0:
             # Calculate xp and level gains
             print("The foe's pokemon has fainted!")
+            time.sleep(0.5)
             print(my_pokemon.gainXp(wild_pokemon.yieldXp))
             lvl_now, xp_now = my_pokemon.evalStat()
             valid = False
@@ -262,7 +295,9 @@ def main():
                 elif stay == 'y':
                     # Initiate current state pokemon and moves
                     my_pokemon = MyPokemon(Pikachu, lvl=lvl_now, xp=xp_now)
-                    wild_pokemon= WildPokemon(random.choice(UniquePoke.pokemonlist), lvl=4)
+                    random_poke = random.choice(list(area.wildict))
+                    lvl_min, lvl_max = area.wildict[random_poke]
+                    wild_pokemon = WildPokemon(random_poke, lvl=random.randint(lvl_min, lvl_max))
                     valid = True
                     print()
                     print("------------------------------")
