1ef670054d28ee1eab69faa2d777c693934ada79
Antonius Anggito <38179060+Gytzero@users.noreply.github.com>
2019-12-27 15:27:03 +0700

    Added stat move and accuracy

--- main.py
+++ main.py
@@ -21,14 +21,19 @@ class Category():
         self.ctgname = ctgname
 
     def __str__(self):
-        return f"({self.ctgname} attack)"
+        return f"({self.ctgname} move)"
 
 class Moves():
-    def __init__(self, movename, movetype, pwr, category):
+    def __init__(self, movename, movetype, pwr, acc, category):
         self.movename = movename
         self.movetype = movetype
         self.pwr = pwr
+        self.acc = acc
         self.category = category
+    
+    def addEffect(self, statchgobj, bystage):
+        self.statchgobj = statchgobj
+        self.bystage = bystage
 
     def getMove(self):
         return self.movename
@@ -44,19 +49,18 @@ class UniquePoke():
 
     pokemonlist = []
 
-    def __init__(self, name, poketype, statlist, movedict):
+    def __init__(self, name, poketype1, poketype2, statlist, movedict):
         self.__class__.pokemonlist.append(weakref.proxy(self))
         self.name = name
-        self.poketype = poketype
+        self.poketype1 = poketype1
+        self.poketype2 = poketype2
         self.xp = 0
-        self.statlist = statlist
         self.bhp = statlist[0]
         self.batk = statlist[1]
         self.bdef = statlist[2]
         self.bspatk = statlist[3]
         self.bspdef = statlist[4]
         self.bspe = statlist[5]
-        self.totalstat = sum(statlist)
         self.movedict = movedict
 
     def __str__(self):
@@ -75,6 +79,7 @@ class MyPokemon(UniquePoke):
         self.sspatk = math.floor((((self.pokeobj.bspatk*2) * self.lvl) / 100) + 5)
         self.sspdef = math.floor((((self.pokeobj.bspdef*2) * self.lvl) / 100) + 5)
         self.sspe = math.floor((((self.pokeobj.bspe*2) * self.lvl) / 100) + 5)
+        self.statlist = [self.shp, self.satk, self.sdef, self.sspatk, self.sspdef, self.sspe]
         self.xp = xp
         self.xplimit = self.lvl * 3
         self.movelist = []
@@ -91,8 +96,8 @@ class MyPokemon(UniquePoke):
 
     def showStat(self):
         for stat in self.statlist:
-            print(stat)
-        print("Total:", self.totalstat)
+            print(stat, end=' ')
+        print("Total:", sum(self.statlist))
 
     def showMoves(self):
         print("Moves available:\n 1.{:20} 3.{}\n 2.{:20} 4.{}".format(str(self.move1), str(self.move3), str(self.move2), str(self.move4)))
@@ -101,28 +106,44 @@ class MyPokemon(UniquePoke):
         move = self.movelist[int(movenum)-1]
         print(f"Your {self.pokeobj.name} used {move} on the foe's pokemon!")
         # STAB calculation
-        if move.movetype == self.pokeobj.poketype:
+        if move.movetype == self.pokeobj.poketype1 or move.movetype == self.pokeobj.poketype1:
             stab = 1.5
         else:
             stab = 1
         # Type effectiveness calculation
-        if foeobj.pokeobj.poketype in move.movetype.supereff:
-            eff = 2
-        elif foeobj.pokeobj.poketype in move.movetype.notveryeff:
-            eff = 0.5
-        elif foeobj.pokeobj.poketype in move.movetype.ineff:
+        if foeobj.pokeobj.poketype1 in move.movetype.ineff or foeobj.pokeobj.poketype2 in move.movetype.ineff:
+            print("It doesn't affect.")
             eff = 0
+        elif foeobj.pokeobj.poketype1 in move.movetype.supereff and foeobj.pokeobj.poketype2 in move.movetype.supereff:
+            print("It's very effective!")
+            eff = 4
+        elif bool(foeobj.pokeobj.poketype1 in move.movetype.supereff) ^ bool(foeobj.pokeobj.poketype2 in move.movetype.supereff):
+            print("It's very effective!")
+            eff = 2
+        elif bool(foeobj.pokeobj.poketype1 in move.movetype.notveryeff) ^ bool(foeobj.pokeobj.poketype2 in move.movetype.notveryeff):
+            print("It's not very effective!")
+            eff = 0.5
+        elif foeobj.pokeobj.poketype1 in move.movetype.notveryeff and foeobj.pokeobj.poketype2 in move.movetype.notveryeff:
+            print("It's not very effective")
+            eff = 0.25
         else:
             eff = 1
         mod = stab * eff
         print(move.category)
         print("Mod =",mod)
-        if move.category == Phy:
-            mydmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.satk/foeobj.sdef) / 50) + 2) * mod)
-            return mydmg
-        elif move.category == Spc:
-            mydmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.sspatk/foeobj.sspdef) / 50) + 2) * mod)
-            return mydmg
+        if random.randrange(0, 100) < move.acc:
+            if move.category == Phy:
+                mydmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.satk/foeobj.sdef) / 50) + 2) * mod)
+                return mydmg
+            elif move.category == Spc:
+                mydmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.sspatk/foeobj.sspdef) / 50) + 2) * mod)
+                return mydmg
+            elif move.category == Stt:
+                tes = assignEffect(move, move.statchgobj, move.bystage)
+                return 0
+        else:
+            print("The attack missed!")
+            return 0
 
     def hpLoss(self, hitdmg):
         self.shp -= hitdmg
@@ -202,6 +223,7 @@ class WildPokemon(UniquePoke):
         self.sspatk = math.floor((((self.pokeobj.bspatk*2) * self.lvl) / 100) + 5)
         self.sspdef = math.floor((((self.pokeobj.bspdef*2) * self.lvl) / 100) + 5)
         self.sspe = math.floor((((self.pokeobj.bspe*2) * self.lvl) / 100) + 5)
+        self.statlist = [self.shp, self.satk, self.sdef, self.sspatk, self.sspdef, self.sspe]
         # Assigning moveset
         self.movelist = []
         for k,v in self.pokeobj.movedict.items():
@@ -215,6 +237,11 @@ class WildPokemon(UniquePoke):
         self.move3 = self.movelist[2]
         self.move4 = self.movelist[3]
 
+    def showStat(self):
+        for stat in self.statlist:
+            print(stat, end=' ')
+        print("Total:", sum(self.statlist))
+
     def showMoves(self):
         return f"Foe's moves:\n 1.{self.move1}\n 2.{self.move2}\n 3.{self.move3}\n 4.{self.move4}"
 
@@ -224,28 +251,41 @@ class WildPokemon(UniquePoke):
         move = self.movelist[movenum-1]
         print(f"Foe's {self.pokeobj.name} used {move} on your pokemon!")
         # STAB calculation
-        if move.movetype == self.pokeobj.poketype:
+        if move.movetype == self.pokeobj.poketype1 or move.movetype == self.pokeobj.poketype1:
             stab = 1.5
         else:
             stab = 1
         # Type effectiveness calculation
-        if myobj.pokeobj.poketype in move.movetype.supereff:
-            eff = 2
-        elif myobj.pokeobj.poketype in move.movetype.notveryeff:
-            eff = 0.5
-        elif myobj.pokeobj.poketype in move.movetype.ineff:
+        if myobj.pokeobj.poketype1 in move.movetype.ineff or myobj.pokeobj.poketype2 in move.movetype.ineff:
+            print("It doesn't affect.")
             eff = 0
+        elif myobj.pokeobj.poketype1 in move.movetype.supereff and myobj.pokeobj.poketype2 in move.movetype.supereff:
+            print("It's very effective!")
+            eff = 4
+        elif bool(myobj.pokeobj.poketype1 in move.movetype.supereff) ^ bool(myobj.pokeobj.poketype2 in move.movetype.supereff):
+            print("It's very effective!")
+            eff = 2
+        elif bool(myobj.pokeobj.poketype1 in move.movetype.notveryeff) ^ bool(myobj.pokeobj.poketype2 in move.movetype.notveryeff):
+            print("It's not very effective!")
+            eff = 0.5
+        elif myobj.pokeobj.poketype1 in move.movetype.notveryeff and myobj.pokeobj.poketype2 in move.movetype.notveryeff:
+            print("It's not very effective")
+            eff = 0.25
         else:
             eff = 1
         mod = stab * eff
         print(move.category)
         print("Mod =", mod)
-        if move.category == Phy:
-            foedmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.satk/myobj.sdef) / 50) + 2) * mod)
-            return foedmg
-        elif move.category == Spc:
-            foedmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.sspatk/myobj.sspdef) / 50) + 2) * mod)
-            return foedmg
+        if random.randrange(0, 100) < move.acc:
+            if move.category == Phy:
+                foedmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.satk/myobj.sdef) / 50) + 2) * mod)
+                return foedmg
+            elif move.category == Spc:
+                foedmg = math.floor((((((2*self.lvl)/5 + 2) * move.pwr * self.sspatk/myobj.sspdef) / 50) + 2) * mod)
+                return foedmg
+        else:
+            print("The attack missed!")
+            return 0
 
     def hpLoss(self, hitdmg):
         self.shp -= hitdmg
@@ -274,28 +314,46 @@ class Area():
     def __str__(self):
         return self.areaname
 
+
 # Type initiations
+Empty = Type("")
 Normal = Type("Normal")
 Fire = Type("Fire")
 Water = Type("Water")
 Electric = Type("Electric")
 Grass = Type("Grass")
+Ice = Type("Ice")
+Fighting = Type("Fighting")
+Poison = Type("Poison")
 Ground = Type("Ground")
 Flying = Type("Flying")
+Psychic = Type("Psychic")
 Bug = Type("Bug")
+Rock = Type("Rock")
+Ghost = Type("Ghost")
+Dragon = Type("Dragon")
 Dark = Type("Dark")
 Steel = Type("Steel")
 
-Normal.setEff([], [Steel], [])
-Fire.setEff([Grass, Bug, Steel], [Fire, Water], [])
-Water.setEff([Fire, Ground], [Water, Grass], [])
-Electric.setEff([Water, Flying], [Electric, Grass], [Ground])
-Grass.setEff([Water, Ground], [Fire, Grass, Flying, Bug, Steel], [])
-Ground.setEff([Fire, Electric, Steel], [Grass], [Flying])
-Flying.setEff([Grass, Bug], [Electric, Steel], [])
-Bug.setEff([Grass, Dark], [Fire, Ground, Steel], [])
-Dark.setEff([], [Dark], [])
-Steel.setEff([], [Fire, Water, Electric, Steel], [])
+# Type effectiveness initiation
+Empty.setEff([], [], [])
+Normal.setEff([], [Rock, Steel], [Ghost])
+Fire.setEff([Grass, Ice, Bug, Steel], [Fire, Water, Rock, Dragon], [])
+Water.setEff([Fire, Ground, Rock], [Water, Grass, Dragon], [])
+Electric.setEff([Water, Flying], [Electric, Grass, Dragon], [Ground])
+Grass.setEff([Water, Ground, Rock], [Fire, Grass, Poison, Flying, Bug, Dragon, Steel], [])
+Ice.setEff([Grass, Ground, Flying, Dragon], [Fire, Water, Ice, Steel], [])
+Fighting.setEff([Normal, Ice, Rock, Dark, Steel], [Poison, Flying, Psychic, Bug], [Ghost])
+Poison.setEff([Grass], [Poison, Ground, Rock, Ghost], [Steel])
+Ground.setEff([Fire, Electric, Poison, Rock, Steel], [Grass, Bug], [Flying])
+Flying.setEff([Grass, Fighting, Bug], [Electric, Rock, Steel], [])
+Psychic.setEff([Fighting, Poison], [Psychic, Steel], [Dark])
+Bug.setEff([Grass, Psychic, Dark], [Fire, Fighting, Poison, Flying, Rock, Steel], [])
+Rock.setEff([Fire, Ice, Flying, Bug], [Fighting, Ground, Steel], [])
+Ghost.setEff([Psychic, Ghost], [Dark], [Normal])
+Dragon.setEff([Dragon], [Steel], [])
+Dark.setEff([Psychic, Ghost], [Fighting, Dark], [])
+Steel.setEff([Ice, Rock], [Fire, Water, Electric, Steel], [])
 
 # Move category initiation
 Phy = Category("Physical")
@@ -303,32 +361,34 @@ Spc = Category("Special")
 Stt = Category("Stat")
 
 # Moves initiation
-Tackle = Moves("Tackle", Normal, 40, Phy)
-Scratch = Moves("Scratch", Normal, 40, Phy)
-Pound = Moves("Pound", Normal, 40, Phy)
-Peck = Moves("Peck", Flying, 45, Phy)
-Metal_Claw = Moves("Metal Claw", Steel, 45, Phy)
-Mud_Shot = Moves("Mud Shot", Ground, 45, Spc)
-Vine_Whip = Moves("Vine Whip", Grass, 45, Phy)
-Ember = Moves("Ember", Fire, 45, Spc)
-Bubble = Moves("Bubble", Water, 45, Spc)
-Thunder_Shock = Moves("Thunder Shock", Electric, 45, Spc)
-Slam = Moves("Slam", Normal, 50, Phy)
-Shock_Wave = Moves("Shock Wave", Electric, 60, Spc)
+Tackle = Moves("Tackle", Normal, 40, 100, Phy)
+Scratch = Moves("Scratch", Normal, 40, 100, Phy)
+Pound = Moves("Pound", Normal, 40, 100, Phy)
+Leer = Moves("Leer", Empty, 0, 100, Stt)
+
+Peck = Moves("Peck", Flying, 45, 100, Phy)
+Metal_Claw = Moves("Metal Claw", Steel, 45, 95, Phy)
+Mud_Shot = Moves("Mud Shot", Ground, 45, 95, Spc)
+Vine_Whip = Moves("Vine Whip", Grass, 45, 95, Phy)
+Ember = Moves("Ember", Fire, 45, 95, Spc)
+Bubble = Moves("Bubble", Water, 45, 95, Spc)
+Thunder_Shock = Moves("Thunder Shock", Electric, 45, 95, Spc)
+Slam = Moves("Slam", Normal, 50, 90, Phy)
+Shock_Wave = Moves("Shock Wave", Electric, 60, 85, Spc)
 
 # Pokemon available initiation
-Pikachu = UniquePoke("Pikachu", Electric, [35, 55, 40, 50, 50, 90], movedict={2:Scratch, 4:Thunder_Shock, 7:Slam, 12:Shock_Wave, 14:Metal_Claw})
-Treecko = UniquePoke("Treecko", Grass, [40, 45, 35, 65, 55, 70], movedict={2:Tackle, 4:Peck, 7:Vine_Whip})
-Torchic = UniquePoke("Torchic", Fire, [45, 60, 40, 70, 50, 45], movedict={2:Scratch, 4:Metal_Claw, 7:Ember})
-Mudkip = UniquePoke("Mudkip", Water, [50, 70, 50, 50, 50, 40], movedict={2:Pound, 4:Mud_Shot, 7:Bubble})
-Poochyena = UniquePoke("Poochyena", Dark, [38, 30, 41, 30, 30, 35], movedict={2:Scratch, 2:Peck, 4:Slam})
-Zigzagoon = UniquePoke("Zigzagoon", Normal, [35, 55, 35, 30, 41, 60], movedict={2:Tackle, 2:Scratch, 4:Pound})
-Wurmple = UniquePoke("Wurmple", Bug, [45, 45, 35, 20, 30, 20], movedict={2:Pound, 2:Tackle, 4:Peck})
+Pikachu = UniquePoke("Pikachu", Electric, Steel, [35, 55, 40, 50, 50, 90], movedict={2:Leer, 4:Thunder_Shock, 7:Peck, 12:Mud_Shot, 14:Shock_Wave})
+Treecko = UniquePoke("Treecko", Dragon, Ground, [40, 45, 35, 65, 55, 70], movedict={2:Tackle, 4:Peck, 7:Vine_Whip})
+Torchic = UniquePoke("Torchic", Fire, Fighting, [45, 60, 40, 70, 50, 45], movedict={2:Scratch, 4:Metal_Claw, 7:Ember})
+Mudkip = UniquePoke("Mudkip", Water, Ground, [50, 70, 50, 50, 50, 40], movedict={2:Pound, 4:Mud_Shot, 7:Bubble})
+Poochyena = UniquePoke("Poochyena", Dark, Ghost, [38, 30, 41, 30, 30, 35], movedict={2:Scratch, 2:Peck, 4:Slam})
+Zigzagoon = UniquePoke("Zigzagoon", Normal, Rock, [35, 55, 35, 30, 41, 60], movedict={2:Tackle, 2:Scratch, 4:Pound})
+Wurmple = UniquePoke("Wurmple", Bug, Poison, [45, 45, 35, 20, 30, 20], movedict={2:Pound, 2:Tackle, 4:Peck})
 
 # Area initiation
 Route_101 = Area("Route 101", {Poochyena:[2,3], Zigzagoon:[2,3]})
 Route_103 = Area("Route 103", {Poochyena:[2,4], Zigzagoon:[2,4], Wurmple:[2,4]})
-Victory_Road = Area("Victory Road", {Zigzagoon:[12,12], Poochyena:[12,12], Treecko:[12,12]})
+Victory_Road = Area("Victory Road", {Treecko:[9,12], Torchic:[9,12], Mudkip:[9,12], Poochyena:[9,12], Zigzagoon:[9,12], Wurmple:[9,12]})
 
 def chooseArea():
     print("Area available:")
@@ -375,6 +435,23 @@ def askQuit():
         else:
             pass
 
+def assignEffect(move, statobj, bystage):
+    print("awal", statobj)
+    if move.bystage < 0:
+        multiplier = 2 / (2+(-move.bystage))
+        print(multiplier)
+        statobj *= multiplier
+        print(statobj)
+        print(f"The {statobj} stat was lowered by {-bystage} stage!")
+        return statobj
+    elif move.bystage > 0:
+        multiplier = (2+move.bystage) / 2
+        print(multiplier)
+        statobj *= multiplier
+        print(statobj)
+        print(f"The {statobj} stat was rose by {bystage} stage!")
+        return statobj
+
 def main():
     # Greetings
     print("\nWelcome to The Pokemon World!")
@@ -399,9 +476,14 @@ def main():
         lvl_min, lvl_max = area.wildict[random_poke]
         wild_pokemon = WildPokemon(random_poke, lvl=random.randint(lvl_min, lvl_max))
 
+        Leer.addEffect(wild_pokemon.sdef, -1)
+
         # Print current pokemon state
         print(f"{my_pokemon} ({my_pokemon.sspe})")
+        my_pokemon.showStat()
+        print()
         print(f"{wild_pokemon} ({wild_pokemon.sspe})")
+        wild_pokemon.showStat()
         print()
         my_pokemon.showHp()
         wild_pokemon.showHp()
